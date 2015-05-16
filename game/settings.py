867e79b5951fa993f9f3fba15def7a3c1a1b555c
Grigory Kruglov <grigory.kruglov@gmail.com>
2015-05-16 15:14:35 -0700

    +x manage.py

--- game/settings.py
+++ game/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # external apps
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +83,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
