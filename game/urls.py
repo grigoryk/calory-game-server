df44fe23aa49e86771390f56eb369af06fe490bc
Grigory Kruglov <grigory.kruglov@gmail.com>
2015-05-16 16:42:58 -0700

    enable dishes api

--- game/urls.py
+++ game/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, include, url
+from rest_framework import routers
+from dishes.api import views
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'game.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+router = routers.DefaultRouter()
+router.register(r'dishes', views.DishViewSet)
+
+urlpatterns = patterns(
+    '',
+
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include(
+        'rest_framework.urls', namespace='rest_framework')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
