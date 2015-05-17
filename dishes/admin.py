fc868758cea4336cd60a4b490d83a1aa0befb75a
Grigory Kruglov <grigory.kruglov@gmail.com>
2015-05-16 17:01:02 -0700

    write out admin in more detail

--- dishes/admin.py
+++ dishes/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 
-from .models import Image, NutritionalData, Dish, Guess
+from .models import (
+    Image, NutritionalDataDish, NutritionalDataGuess, Dish, Guess)
+
+
+class NutritionalDataDishInline(admin.StackedInline):
+    model = NutritionalDataDish
+
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('description', 'is_vegetarian', 'created_at')
+    inlines = [NutritionalDataDishInline]
+
+
+class NutritionalDataGuessInline(admin.StackedInline):
+    model = NutritionalDataGuess
+
+
+class GuessAdmin(admin.ModelAdmin):
+    inlines = [NutritionalDataGuessInline]
+
 
 admin.site.register(Image)
-admin.site.register(NutritionalData)
-admin.site.register(Dish)
-admin.site.register(Guess)
+admin.site.register(Dish, DishAdmin)
+admin.site.register(Guess, GuessAdmin)
